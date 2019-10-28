7588efd224c4485d4a9c014472ca9e39b374fc6b
fe3o4tmh <tmh.hung.97@gmail.com>
2019-10-28 13:17:09 +0700

    no message

--- ui/login/Login.aspx.cs
+++ ui/login/Login.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,29 +10,49 @@ namespace main
 {
     public partial class Login : System.Web.UI.Page
     {
-        private model.Login mLogin;
         protected void Page_Load(object sender, EventArgs e)
         {
-            mLogin = model.Login.GetInstant;
+            model.Login.GetInstant.strConnection = @"C:\USERS\BEART\DESKTOP\CNPM\APP_DATA\DATABASE.MDF";
             fGetView();
         }
 
         private void fGetView()
         {
-            mLogin.email = login_email.Value;
-            mLogin.password = login_passowrd.Value;
+            model.Login.GetInstant.email = login_email.Value;
+            model.Login.GetInstant.password = login_passowrd.Value;
         }
 
         private void login(object sender, EventArgs e)
         {
-            if (checkUI(mLogin.email, mLogin.password))
+            if (checkUI(model.Login.GetInstant.email, model.Login.GetInstant.password))
             {
+                using (SqlConnection cn = new SqlConnection(model.Login.GetInstant.strConnection))
+                {
+                    cn.Open();
+                    SqlCommand com;
+                    SqlDataReader reader;
+                    String sql;
+                    Boolean result = false;
 
+                    sql = "SELECT * from tbl_tai_khoan where tbl_tai_khoan.emal = " + model.Login.GetInstant.email + "and tbl_tai_khoan.matkhau = " + model.Login.GetInstant.password;
+                    com = new SqlCommand(sql, cn);
+
+                    reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (reader.GetValue.ToString.Equals(""))
+                        {
+
+                        }
+                    }
+                    cn.Close();
+                }
             }
         }
         private Boolean checkUI(String email, String password)
         {
-           if(checkEmail(email) && checkPassword(password)){
+            if (!checkEmail(email).Equals("") && !checkPassword(password).Equals(""))
+            {
                 return true;
             }
             return false;
